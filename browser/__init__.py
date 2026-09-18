4d674b37ddf15b5bf37e52d69f246eb8c00c07a9
maha10092007-stack <maha10092007@gmail.com>
2026-09-18 10:59:53 -0700

    Add files via upload

---
+++ browser/__init__.py
@@ -0,0 +1,6 @@
+from .browser_manager import open_browser, close_browser
+from .navigation import open_website
+from .actions import click_element, type_text
+from .extraction import get_page_text
+from .search import search_google
+from .browser_controller import BrowserController
